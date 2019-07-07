661d350a75b4b0acd0eb740faf5a0127d71a60d6
jnaputi <jnaputi253@gmail.com>
2019-07-07 13:15:17 -0500

    Tightened visibility levels in the Startup class

--- IntakeTracker/Startup.cs
+++ IntakeTracker/Startup.cs
@@ -6,14 +6,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace IntakeTracker
 {
-    public class Startup
+    class Startup
     {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
